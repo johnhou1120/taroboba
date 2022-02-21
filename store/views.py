6c9afee2bc1b61ac000269558df468fdf952fe2a
johnhou1120 <ohn980071@gmail.com>
2022-02-21 19:36:22 +0800

    add templates and static

--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http.response import HttpResponse
+from django.template.loader import get_template
 # Create your views here.
+
+def index(request):
+    template = get_template('index.html')
+    html = template.render(locals())
+    return render(request, 'index.html', locals())
+
+def menu(request):
+    template = get_template('menu.html')
+    html = template.render(locals())
+    return render(request, 'menu.html', locals())
--- taroboba/urls.py
+++ taroboba/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from store.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('menu/', menu, name='menu'),
 ]
